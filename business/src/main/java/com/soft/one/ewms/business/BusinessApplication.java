5b43d24d86b5709c9e137e79d4844de7a814597d
wzq-55552 <you@example.com>
2020-01-04 11:24:24 +0800

    大体项目框架--吴泽强

---
+++ business/src/main/java/com/soft/one/ewms/business/BusinessApplication.java
@@ -0,0 +1,15 @@
+package com.soft.one.ewms.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan(basePackages = "com.soft.one.ewms.business.mapper")
+@ServletComponentScan
+@SpringBootApplication
+public class BusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+}
