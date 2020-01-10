8d67f6a996aa80764e9c909c49274d5745c7bee9
fanlihuang <1633289851@qq.com>
2020-01-10 14:36:41 +0800

    获取当前计算机唯一表示MacAddress

--- business/src/main/java/com/soft/one/ewms/business/BusinessApplication.java
+++ business/src/main/java/com/soft/one/ewms/business/BusinessApplication.java
@@ -1,9 +1,11 @@
 package com.soft.one.ewms.business;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
+
 
 @MapperScan(basePackages = {"com.soft.one.ewms.business.account.mapper","com.soft.one.ewms.business.permission.mapper"})
 @ServletComponentScan
